024c9288a137c7f455094df156659e3302b51bcb
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2025-04-08 00:30:42 +0530

    fix import statements for side menu and summary menu widgets

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,6 @@
 import 'package:fitness_tracker/widgets/dashboard.dart';
-import 'package:fitness_tracker/widgets/side-menu.dart';
-import 'package:fitness_tracker/widgets/summary-menu.dart';
+import 'package:fitness_tracker/widgets/side_menu.dart';
+import 'package:fitness_tracker/widgets/summary_menu.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
