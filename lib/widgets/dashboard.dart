24a4bd7c1e8e66184dafe14dd1232446dc40461b
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2025-04-08 00:08:23 +0530

    add Dashboard widget with basic layout

---
+++ lib/widgets/dashboard.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class Dashboard extends StatefulWidget {
+  const Dashboard({super.key});
+
+  @override
+  State<Dashboard> createState() => _DashboardState();
+}
+
+class _DashboardState extends State<Dashboard> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          "Dashboard",
+          style: TextStyle(
+            fontSize: 30,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+}
