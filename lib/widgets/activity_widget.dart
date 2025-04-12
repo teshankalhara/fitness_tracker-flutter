9577d21a99270a1eab75608a186d506f204f1fbd
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2025-04-13 00:52:08 +0530

    add ActivityWidget to display activity placeholder in the app

---
+++ lib/widgets/activity_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ActivityWidget extends StatelessWidget {
+  const ActivityWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
