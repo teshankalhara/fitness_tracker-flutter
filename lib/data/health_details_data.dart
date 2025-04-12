31790b056b6ce00148500acf45418f7ab587d37d
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2025-04-13 01:00:52 +0530

    add HealthDetails class to manage health activity data

---
+++ lib/data/health_details_data.dart
@@ -0,0 +1,22 @@
+import 'package:fitness_tracker/models/health_activity_model.dart';
+
+class HealthDetails {
+  final healthData = const [
+    HealthModel(
+      icon: 'assets/icons/burn.png',
+      value: "230",
+      title: "Water Level",
+    ),
+    HealthModel(
+      icon: 'assets/icons/steps.png',
+      value: "7.8k",
+      title: "Miles  Steps",
+    ),
+    HealthModel(
+      icon: 'assets/icons/distance.png',
+      value: "340m",
+      title: "Covered distance",
+    ),
+    HealthModel(icon: 'assets/icons/sleep.png', value: "7h48m", title: "Sleep"),
+  ];
+}
