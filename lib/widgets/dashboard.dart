8bf4948c84ac9bff682a3e9710abf1edc8b7f253
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2025-04-13 13:37:59 +0530

    add: include BarGraphCard in the Dashboard widget layout

--- lib/widgets/dashboard.dart
+++ lib/widgets/dashboard.dart
@@ -1,4 +1,5 @@
 import 'package:fitness_tracker/widgets/activity_widget.dart';
+import 'package:fitness_tracker/widgets/bar_graph_widget.dart';
 import 'package:fitness_tracker/widgets/header_widget.dart';
 import 'package:fitness_tracker/widgets/line_chart.dart';
 import 'package:flutter/material.dart';
@@ -24,6 +25,9 @@ class _DashboardState extends State<Dashboard> {
             ActivityDetails(),
             SizedBox(height: 15),
             LineChartCard(),
+            SizedBox(height: 15),
+            BarGraphCard(),
+            SizedBox(height: 15),
           ],
         ),
       ),
