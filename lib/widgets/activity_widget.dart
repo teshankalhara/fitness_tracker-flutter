c19ce5191df7eaabb757046b7ee3e97106fd4854
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2025-04-13 10:31:54 +0530

    refactor ActivityWidget to ActivityDetails and implement grid view for health data display

--- lib/widgets/activity_widget.dart
+++ lib/widgets/activity_widget.dart
@@ -1,10 +1,57 @@
+import 'package:fitness_tracker/data/health_details_data.dart';
+import 'package:fitness_tracker/utils/responsive.dart';
+import 'package:fitness_tracker/widgets/custum_card.dart';
 import 'package:flutter/material.dart';
 
-class ActivityWidget extends StatelessWidget {
-  const ActivityWidget({super.key});
+class ActivityDetails extends StatelessWidget {
+  const ActivityDetails({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final healthModalData = HealthDetails();
+
+    return GridView.builder(
+      itemCount: healthModalData.healthData.length,
+      shrinkWrap: true,
+      physics: const ScrollPhysics(),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: Responsive.isMobile(context) ? 2 : 4,
+        crossAxisSpacing: Responsive.isMobile(context) ? 12 : 15,
+        mainAxisSpacing: 12,
+      ),
+      itemBuilder:
+          (context, index) => CustomCard(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Image.asset(
+                  healthModalData.healthData[index].icon,
+                  width: 30,
+                  height: 30,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 15, bottom: 4),
+                  child: Text(
+                    healthModalData.healthData[index].value,
+                    style: const TextStyle(
+                      fontSize: 18,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+                Text(
+                  healthModalData.healthData[index].title,
+                  style: const TextStyle(
+                    fontSize: 13,
+                    color: Colors.grey,
+                    fontWeight: FontWeight.normal,
+                  ),
+                ),
+              ],
+            ),
+          ),
+    );
   }
 }
