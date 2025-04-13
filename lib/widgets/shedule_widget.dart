1ad8dde795a72822d98cf24b9488bb6d2063b93b
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2025-04-13 13:43:20 +0530

    refactor: improve layout structure of ScheduledWidget for better readability

--- lib/widgets/shedule_widget.dart
+++ lib/widgets/shedule_widget.dart
@@ -10,59 +10,61 @@ class ScheduledWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     final data = ScheduleTasksData();
 
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        const Text(
-          "Scheduled",
-          style: TextStyle(
-            fontSize: 16,
-            fontWeight: FontWeight.w500,
-            color: whiteColor,
-          ),
-        ),
-        const SizedBox(height: 12),
-        for (var index = 0; index < data.scheduled.length; index++)
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 5),
-            child: CustomCard(
-              color: limeColor,
-              child: Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            data.scheduled[index].title,
-                            style: const TextStyle(
-                              fontSize: 12,
-                              fontWeight: FontWeight.w500,
-                              color: whiteColor,
-                            ),
-                          ),
-                          const SizedBox(height: 2),
-                          Text(
-                            data.scheduled[index].date,
-                            style: const TextStyle(
-                              fontSize: 12,
-                              color: greyColor,
-                              fontWeight: FontWeight.w500,
-                            ),
-                          ),
-                        ],
-                      ),
-                      const Icon(Icons.more, color: greyColor),
-                    ],
-                  ),
-                ],
-              ),
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            "Scheduled",
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.w500,
+              color: whiteColor,
             ),
           ),
-      ],
+          const SizedBox(height: 12),
+          for (var index = 0; index < data.scheduled.length; index++)
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 5),
+              child: CustomCard(
+                color: limeColor,
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              data.scheduled[index].title,
+                              style: const TextStyle(
+                                fontSize: 12,
+                                fontWeight: FontWeight.w500,
+                                color: whiteColor,
+                              ),
+                            ),
+                            const SizedBox(height: 2),
+                            Text(
+                              data.scheduled[index].date,
+                              style: const TextStyle(
+                                fontSize: 12,
+                                color: greyColor,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ],
+                        ),
+                        const Icon(Icons.more, color: greyColor),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+        ],
+      ),
     );
   }
 }
