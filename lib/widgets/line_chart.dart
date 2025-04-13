532c9415899ff5ad4e6ad28026a2b9fd3ebfceae
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2025-04-13 13:07:16 +0530

    add LineChartCard widget for displaying step data in a line chart

---
+++ lib/widgets/line_chart.dart
@@ -0,0 +1,103 @@
+import 'package:fitness_tracker/constant/color.dart';
+import 'package:fitness_tracker/data/line_chart_data.dart';
+import 'package:fitness_tracker/widgets/custum_card.dart';
+import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/material.dart';
+
+class LineChartCard extends StatelessWidget {
+  const LineChartCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final data = LineData();
+
+    return CustomCard(
+      child: Column(
+        children: [
+          Text(
+            "Steps Overview",
+            style: TextStyle(fontWeight: FontWeight.w600, color: greyColor),
+          ),
+          SizedBox(height: 20),
+          AspectRatio(
+            aspectRatio: 16 / 6,
+            child: LineChart(
+              LineChartData(
+                lineTouchData: LineTouchData(handleBuiltInTouches: true),
+                titlesData: FlTitlesData(
+                  rightTitles: AxisTitles(
+                    sideTitles: SideTitles(showTitles: false),
+                  ),
+                  topTitles: AxisTitles(
+                    sideTitles: SideTitles(showTitles: false),
+                  ),
+                  bottomTitles: AxisTitles(
+                    sideTitles: SideTitles(
+                      showTitles: true,
+                      getTitlesWidget: (double value, TitleMeta meta) {
+                        return data.bottomTitle[value.toInt()] != null
+                            ? SideTitleWidget(
+                              meta: meta,
+                              child: Text(
+                                data.bottomTitle[value.toInt()].toString(),
+                                style: const TextStyle(
+                                  fontSize: 12,
+                                  color: greyColor,
+                                ),
+                              ),
+                            )
+                            : const SizedBox();
+                      },
+                    ),
+                  ),
+                  leftTitles: AxisTitles(
+                    sideTitles: SideTitles(
+                      showTitles: true,
+                      interval: 1,
+                      reservedSize: 40,
+                      getTitlesWidget: (double value, TitleMeta meta) {
+                        return data.leftTitle[value.toInt()] != null
+                            ? Text(
+                              data.leftTitle[value.toInt()].toString(),
+                              style: const TextStyle(
+                                fontSize: 12,
+                                color: greyColor,
+                              ),
+                            )
+                            : const SizedBox();
+                      },
+                    ),
+                  ),
+                ),
+                borderData: FlBorderData(show: false),
+                lineBarsData: [
+                  LineChartBarData(
+                    color: selectionColor,
+                    barWidth: 3,
+                    belowBarData: BarAreaData(
+                      show: true,
+                      gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: [
+                          selectionColor.withOpacity(0.5),
+                          Colors.transparent,
+                        ],
+                      ),
+                    ),
+                    dotData: const FlDotData(show: false),
+                    spots: data.spots,
+                  ),
+                ],
+                minX: 0,
+                maxX: 120,
+                maxY: 105,
+                minY: -5,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
